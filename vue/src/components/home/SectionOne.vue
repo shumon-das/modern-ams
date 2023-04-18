571cd418f60605fd7df5d1205bd010b57aab12f9
monoranjan.das <monoranjan.das@covianalytics.com>
2023-04-18 12:11:42 +0600

    fixed section one part-one

--- vue/src/components/home/SectionOne.vue
+++ vue/src/components/home/SectionOne.vue
@@ -4,18 +4,18 @@
         <img src="@/assets/images/section-one-handshake.jpg" alt="" class="back-img">
       </div>
 
-    <div class="col-2"></div>
-    <div class="col-4 sec-one-one">
+    <div class="col-1"></div>
+    <div class="col-5 sec-one-one mt-6">
       <div class="service-message">
         <h3 class="section-one-message text-7xl">Unified asset management service for your company</h3>
         <h2 class="section-one-message">Design, automate, deliver, and manage critical IT and business services</h2>
         <AmsButton class=" block m-auto" label="TRY FOR A FREE DEMO" severity="help" raised  />
       </div>
     </div>
-    <div class="col-4 sec-one-one">
-<!--      <img src="@/assets/images/section-one-pc-user.jpg" alt="">-->
+    <div class="col-5 sec-one-one mt-6">
+      <img class="sec-one-img2" src="@/assets/images/group-work.png" alt="">
     </div>
-    <div class="col-2"></div>
+    <div class="col-1"></div>
   </div>
 </template>
 
@@ -44,5 +44,9 @@ export default defineComponent({
 }
 .section-one-message {
   font-family: sans-serif;
+}
+.sec-one-img2 {
+  width: 100%;
+  opacity: 0.8;
 }
 </style>
