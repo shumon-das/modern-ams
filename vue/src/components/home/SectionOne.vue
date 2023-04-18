40919b4d91c638ac4c1c32c15856b8987e763b10
monoranjan.das <monoranjan.das@covianalytics.com>
2023-04-18 11:48:12 +0600

    fixed section one part-one

--- vue/src/components/home/SectionOne.vue
+++ vue/src/components/home/SectionOne.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="grid section-one">
+      <div class="img">
+        <img src="@/assets/images/section-one-handshake.jpg" alt="" class="back-img">
+      </div>
+
     <div class="col-2"></div>
-    <div class="col-4">
+    <div class="col-4 sec-one-one">
       <div class="service-message">
         <h3 class="section-one-message text-7xl">Unified asset management service for your company</h3>
         <h2 class="section-one-message">Design, automate, deliver, and manage critical IT and business services</h2>
         <AmsButton class=" block m-auto" label="TRY FOR A FREE DEMO" severity="help" raised  />
       </div>
     </div>
-    <div class="col-4">
-      <img src="@/assets/images/section-one-pc-user.jpg" alt="">
+    <div class="col-4 sec-one-one">
+<!--      <img src="@/assets/images/section-one-pc-user.jpg" alt="">-->
     </div>
     <div class="col-2"></div>
   </div>
@@ -19,17 +23,24 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "SectionOne"
+  name: "SectionOne",
 })
 </script>
 
 <style>
-.section-one {
-  background-image: url("@/assets/images/section-one-handshake.jpg");
-  background-size: cover;
-  height: 100vh;
-  background-repeat: no-repeat;
-  opacity: 0.5;
+
+.img {
+  width: 100%;
+  height: 92vh;
+  position: absolute;
+  opacity: 0.4;
+}
+.back-img {
+  width: 100%;
+  height: 92vh;
+}
+.sec-one-one {
+  position: relative;
 }
 .section-one-message {
   font-family: sans-serif;
